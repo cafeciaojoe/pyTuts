917c2779ab3f3f7047937ca759356d1e5f6b7129
joseph la delfa <cafeciaojoe>
2024-04-19 10:27:40 +0200

    starting to build a real time "drone" plot with synthetic data

---
+++ VisPy/hyper_basic.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def _generate_random_line_positions(num_points, dtype=np.float32):
+    rng = np.random.default_rng()
+    pos = np.empty((num_points, 3), dtype=np.float32)
+    pos[:, 0] = np.arange(num_points)
+    pos[:, 1] = rng.random((num_points,), dtype=dtype)
+    pos[:, 2] = rng.random((num_points,), dtype=dtype)
+    return print(pos)
+
+_generate_random_line_positions(10, dtype=np.float32)
