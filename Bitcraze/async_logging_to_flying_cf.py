360e1132fe8c30c5b5935f8bae9cb73956826843
joseph la delfa <cafeciaojoe>
2025-02-06 12:12:20 +0100

    example script now functional

--- Bitcraze/async_logging_to_flying_cf.py
+++ Bitcraze/async_logging_to_flying_cf.py
@@ -7,13 +7,62 @@ from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.crazyflie.syncLogger import SyncLogger
 
-# URI to the Crazyflie to connect to
+# URIs to the Crazyflie to connect to
 uri = 'radio://0/80/2M/A0A0A0A0A9'
+uri_2 = 'radio://0/80/2M/A0A0A0A0AC'
 latest_cf_sensor_data = False
-
 
 # Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)
+
+def wait_for_position_estimator(scf):
+    print('Waiting for estimator to find position...')
+
+    log_config = LogConfig(name='Kalman Variance', period_in_ms=500)
+    log_config.add_variable('kalman.varPX', 'float')
+    log_config.add_variable('kalman.varPY', 'float')
+    log_config.add_variable('kalman.varPZ', 'float')
+
+    var_y_history = [1000] * 10
+    var_x_history = [1000] * 10
+    var_z_history = [1000] * 10
+
+    threshold = 0.001
+
+    with SyncLogger(scf, log_config) as logger:
+        for log_entry in logger:
+            data = log_entry[1]
+
+            var_x_history.append(data['kalman.varPX'])
+            var_x_history.pop(0)
+            var_y_history.append(data['kalman.varPY'])
+            var_y_history.pop(0)
+            var_z_history.append(data['kalman.varPZ'])
+            var_z_history.pop(0)
+
+            min_x = min(var_x_history)
+            max_x = max(var_x_history)
+            min_y = min(var_y_history)
+            max_y = max(var_y_history)
+            min_z = min(var_z_history)
+            max_z = max(var_z_history)
+
+            # print("{} {} {}".
+            #       format(max_x - min_x, max_y - min_y, max_z - min_z))
+
+            if (max_x - min_x) < threshold and (
+                    max_y - min_y) < threshold and (
+                    max_z - min_z) < threshold:
+                break
+
+
+def reset_estimator(scf):
+    cf = scf.cf
+    cf.param.set_value('kalman.resetEstimation', '1')
+    time.sleep(0.1)
+    cf.param.set_value('kalman.resetEstimation', '0')
+
+    wait_for_position_estimator(cf)
 
 def log_stab_callback(timestamp, data, logconf):
     #print('[%d][%s]: %s' % (timestamp, logconf.name, data))
@@ -23,7 +72,21 @@ def log_stab_callback(timestamp, data, logconf):
     latest_cf_sensor_data['timestamp'] = timestamp
     #print(timestamp)
 
-def simple_log_async(scf, logconf):
+def send_drone(scf, x, y, z, yaw):
+        cf = scf.cf
+        cf.commander.send_position_setpoint(x,y,z,yaw)
+        time.sleep(0.1)
+
+        #cf.commander.send_stop_setpoint()
+        # Hand control over to the high level commander to avoid timeout and locking of the Crazyflie
+        #cf.commander.send_notify_setpoint_stop()
+
+        # Make sure that the last packet leaves before the link is closed
+        # since the message queue is not flushed before closing
+        #time.sleep(0.1)
+
+
+def simple_log_async(scf,scf_2,logconf):
     cf = scf.cf
     cf.log.add_config(logconf)
     logconf.data_received_cb.add_callback(log_stab_callback)
@@ -33,24 +96,32 @@ def simple_log_async(scf, logconf):
     while True:
         if latest_cf_sensor_data is not False:
             if last_timestamp < latest_cf_sensor_data['timestamp']:
-                #print(latest_cf_sensor_data['timestamp'])
-                
+                print(latest_cf_sensor_data['timestamp'])
+                #send_drone(scf_2,0,0,0.5,0)
+                """IMPORTANT add or subtract the right buffer to avoid sending the drone directly into the sensor"""
+                send_drone(scf_2,latest_cf_sensor_data['stateEstimate.x'],
+                           latest_cf_sensor_data['stateEstimate.y']+1,
+                           latest_cf_sensor_data['stateEstimate.z'],
+                           latest_cf_sensor_data['stateEstimate.yaw'])
                 last_timestamp = latest_cf_sensor_data['timestamp']
     logconf.stop()
 
+    
 if __name__ == '__main__':
     # Initialize the low-level drivers
     cflib.crtp.init_drivers()
 
-    lg_stab = LogConfig(name='stateEstimate', period_in_ms=1000)
-    lg_stab.add_variable('stateEstimate.x', 'float')
-    lg_stab.add_variable('stateEstimate.y', 'float')
-    lg_stab.add_variable('stateEstimate.z', 'float')
+    log_state_est = LogConfig(name='stateEstimate', period_in_ms=10)
+    log_state_est.add_variable('stateEstimate.x', 'float')
+    log_state_est.add_variable('stateEstimate.y', 'float')
+    log_state_est.add_variable('stateEstimate.z', 'float')
 
-    lg_stab.add_variable('stateEstimate.roll', 'float')
-    lg_stab.add_variable('stateEstimate.pitch', 'float')
-    lg_stab.add_variable('stateEstimate.yaw', 'float')
+    log_state_est.add_variable('stateEstimate.roll', 'float')
+    log_state_est.add_variable('stateEstimate.pitch', 'float')
+    log_state_est.add_variable('stateEstimate.yaw', 'float')
 
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf_2:
-            simple_log_async(scf, lg_stab, scf_2)
+        with SyncCrazyflie(uri_2, cf=Crazyflie(rw_cache='./cache')) as scf_2:
+            reset_estimator(scf)
+            reset_estimator(scf_2)
+            simple_log_async(scf, scf_2, log_state_est)
